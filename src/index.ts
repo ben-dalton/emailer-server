fa54bd12e598c91442a59682afe05a6f78e4bbce
ben-dalton <benadalton@gmail.com>
2018-04-23 22:19:21 -0400

    reorder index file

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,16 @@ import app from './App';
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.send({
+    hey: 'there',
+  });
+});
+
 app.listen(PORT, (err: any) => {
   if (err) {
     return console.log(err);
   }
 
   return console.log(`server is listening on ${PORT}`);
-});
-
-app.get('/', (req, res) => {
-  res.send({
-    hey: 'there',
-  });
 });
