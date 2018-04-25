9efd6e7420a46df5e6281ed7aa41b5a52e3bdcfe
ben-dalton <benadalton@gmail.com>
2018-04-25 09:20:32 -0400

    adds google oauth

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,46 @@
-// import app from './App';
 import * as express from 'express';
+import * as passport from 'passport';
+import {
+  Strategy as GoogleStrategy,
+  StrategyOptions,
+  VerifyFunction,
+} from 'passport-google-oauth20';
+import { app } from './App';
+import { keys } from './config/keys';
 
-const app = express();
+const BASE_URL = 'http://localhost:5000/';
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send({
-    hey: 'there',
-  });
-});
+const options: StrategyOptions = {
+  ...keys,
+  callbackURL: '/auth/google/callback',
+};
+
+const verify: VerifyFunction = (
+  accessToken: string,
+  refreshToken: string,
+  profile: any,
+) => {
+  console.log('token:', accessToken);
+  console.log('refresh token:', refreshToken);
+  console.log('profile:', profile);
+};
+
+passport.use(new GoogleStrategy(options, verify));
+
+app.use(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  }),
+);
+
+app.use('/auth/google/callback', passport.authenticate('google'));
 
 app.listen(PORT, (err: any) => {
   if (err) {
     return console.log(err);
   }
-
   return console.log(`server is listening on ${PORT}`);
 });
